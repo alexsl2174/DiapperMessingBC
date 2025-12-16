d2efdf13e602f953227eddcc0fe7b70b3a51313c
alexsl2174 <79280782+alexsl2174@users.noreply.github.com>
2025-12-15 22:56:51 -0500

    Update BC Diaper Wetter Loader.js

--- BC Diaper Wetter Loader.js
+++ BC Diaper Wetter Loader.js
@@ -1,25 +1,25 @@
 // ==UserScript==
-// @name         BC Diaper Wetter Loader (Final SDK Fix)
+// @name         BC Diaper Wetter Developmental
 // @namespace    https://www.bondageprojects.com/
-// @version      1.0.3
-// @description  Loads the mod with permissions to access the Mod SDK.
-// @author       Arctic Line / Coding Partner
-// @match        https://bondageprojects.elementfx.com/*
-// @match        https://www.bondageprojects.elementfx.com/*
-// @match        https://bondage-europe.com/*
-// @match        https://www.bondage-europe.com/*
-// @match        http://localhost:*/*
-// @run-at       document-end
-// @grant        unsafeWindow  
+// @version      0.1.1
+// @description  A simple script that will automatically make a baby use their diaper
+// @author       Arctic Line
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match http://localhost:*/*
+// @icon data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant none
+// @run-at document-end
 // ==/UserScript==
 
 (function() {
     'use strict';
-    
-    var script = unsafeWindow.document.createElement("script");
-    
-    // **USING THE SHORTER, MORE RELIABLE RAW URL**
-    script.src = "https://raw.githubusercontent.com/alexsl2174/DiapperMessingBC/main/bcdw_bundle.js"; 
-    
-    unsafeWindow.document.head.appendChild(script);
+
+    let n = document.createElement("script");
+    n.setAttribute("language", "JavaScript");
+    n.setAttribute("crossorigin", "anonymous");
+    n.setAttribute("src", "https://raw.githubusercontent.com/alexsl2174/DiapperMessingBC/main/bcdw_bundle.js" + Date.now());
+    document.head.appendChild(n);
 })();
