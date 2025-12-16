b8bb7da0a9cb62e2dda7d0db92af52073c01662a
alexsl2174 <79280782+alexsl2174@users.noreply.github.com>
2025-12-15 20:10:44 -0500

    Create bcdw-tables.js

---
+++ bcdw-tables.js
@@ -0,0 +1,9 @@
+const BCDWVari =
+{
+    DiaperUseLevels: [["#808080", "#97916A", "#8B8D41"], ["#877C6C", "#7E774E"], ["#4C3017"]]
+};
+
+const BCDWCONST =
+{
+    diaperDefaultValues: { messChance: .3, wetChance: .5, baseTimer: 30, regressionLevel: 0, desperationLevel: 0, messLevelInner: 0, wetLevelInner: 0, messLevelOuter: 0, wetLevelOuter: 0 }
+};
